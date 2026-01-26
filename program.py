86007f3ce493d683221beec7edddfb31cf5ec7aa
Yoav <yoavsarig4@gmail.com>
2026-01-26 16:55:57 +0200

    not working 0.1

---
+++ program.py
@@ -0,0 +1,59 @@
+__author__ = "yoav"
+
+import socket, sys, traceback, threading
+from hashlib import sha256
+from vigenere import encode, decode, find_stdispNmod
+from datetime import datetime, timezone
+
+
+stopcond = False
+prev_key = ''
+current_key = ''
+
+def create_code():
+    global current_key
+    hourly_code = datetime.now(timezone.utc).isoformat()
+    hourly_code = hourly_code[:-19]
+    hourly_code = sha256(hourly_code.encode('utf-8')).hexdigest()
+    hourly_code = sha256(hourly_code.encode('utf-8')).hexdigest()
+    hourly_code = encode(hourly_code,hourly_code[:4])
+    return hourly_code
+
+def craft_resp():
+    pass
+
+def parse_resp():
+    pass
+
+
+def handle_conn(sock):
+    sock = sock[0]
+    sock.send()
+
+
+def main(serip,serport):
+    serversock = socket.socket()
+    serversock.bind((serip,serport))
+    serversock.listen(1000000000)
+    threads = []
+    while not stopcond:
+        connection, address = serversock.accept()
+        t1 = threading.Thread(target=handle_conn, args=(connection,))
+        threads.append(t1)
+        t1.start()
+    for t in threads:
+        t.join()
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        main("127.0.0.1",16000)
+    else:
+        main(sys.argv[1],sys.argv[2])
---
+++ vigenere.py
@@ -0,0 +1,83 @@
+__author__ = 'U(196P8V' #this is my username ('F-162A7V') encrypted in vigenere. try making a code to find the key!
+
+
+
+def find_stdispNmod(word,i):
+    idx = i%len(word)
+    l1 = list('!"#$%&()\'*+,-./')
+    l2 = list(':;<=>?@')
+    l3 = list('[\\]^_`')
+    if word[idx].isupper():
+        stddisp = 65
+        mod = 26
+    elif word[idx].islower():
+        stddisp = 97
+        mod = 26
+    elif word[idx].isdigit():
+        stddisp = 49
+        mod = 10
+    elif word[idx] in l1:
+        stddisp = 33
+        mod = 16
+    elif word[idx] in l2:
+        stddisp = 58
+        mod = 7
+    elif word[idx] in l3:
+        stddisp = 32
+        mod = 6
+    return stddisp, mod
+
+def encode(word, key):
+    new_word = ""
+    key = key.lower()
+    offset = 0
+    for i in range(len(word)):
+        if word[i] != ' ' or word[i] != '|':
+
+            stddisp, mod = find_stdispNmod(word,i)
+            row = ord(word[i]) - stddisp
+            col = ord(key[(i+offset) % len(key)]) - find_stdispNmod(key,i)[0]
+            new_word += chr(stddisp + (row + col) % mod)
+        else:
+            offset -= 1;
+            new_word += ' '
+    return new_word
+
+
+def decode(word, key):
+    decoded_word = ""
+    key = key.lower()
+    offset = 0
+    for i in range(len(word)):
+        if word[i] != ' ' or word[i] != '|':
+            stddisp, mod = find_stdispNmod(word,i)
+            row = ord(word[i]) - stddisp
+            col = ord(key[(i+offset) % len(key)]) - find_stdispNmod(key,i)[0]
+            decoded_word += chr(stddisp + ((row - col) % mod))
+        else:
+            offset -= 1;
+            decoded_word += ' '
+    return decoded_word
+
+
+def main():
+    while True:
+        print("----------------------")
+        print("1: Encode plain text\n")
+        print("2: Decode encrypted text\n")
+        inpt = input("Enter choice: ")
+        if (inpt.isdigit()):
+            text = input("Enter text: ")
+            keyword = input("Enter keyword: ")
+            if inpt == "1":
+                print("Encoded text: " + encode(text, keyword))
+            elif inpt == "2":
+                print("Decoded text: " + decode(text, keyword))
+            else:
+                print("Invalid input")
+        else:
+            print("Invalid input")
+
+
+if __name__ == "__main__":
+    main()
